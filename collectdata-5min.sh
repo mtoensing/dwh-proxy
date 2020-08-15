18735fe7136f1381c2e4e4cd9f57a8903fe52a74
Marc Tönsing <marc@marc.tv>
2020-08-15 14:20:59 +0200

    TZ

--- collectdata-5min.sh
+++ collectdata-5min.sh
@@ -2,4 +2,4 @@
 tps=$(</volume2/SSD/scripts/data/tps.txt)
 cpu=$(</volume2/SSD/scripts/data/cpu.txt)
 mem=$(</volume2/SSD/scripts/data/mem.txt)
-docker run --rm --name collectdata-5min -v /volume2/SSD/scripts/dwh-docker:/usr/src/myapp -w /usr/src/myapp  marctv/python-pymysql:latest sh -c "python dwhproxy.py writeweather 'Hannover,de' && python dwhproxy.py writeblog && python dwhproxy.py writemcserver && python dwhproxy.py writemctps '${tps}' && python dwhproxy.py writemccpu '${cpu}' && python dwhproxy.py writemcmem '${mem}'"
+docker run --rm --name collectdata-5min  -v /etc/localtime:/etc/localtime:ro -v /etc/TZ:/etc/timezone:ro -e TZ=Europe/Berlin -v /volume2/SSD/scripts/dwh-docker:/usr/src/myapp -w /usr/src/myapp  marctv/python-pymysql:latest sh -c "python dwhproxy.py writeweather 'Hannover,de' && python dwhproxy.py writeblog && python dwhproxy.py writemcserver && python dwhproxy.py writemctps '${tps}' && python dwhproxy.py writemccpu '${cpu}' && python dwhproxy.py writemcmem '${mem}'"
